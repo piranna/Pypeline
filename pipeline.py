b445200acbc4dc1c4434b43a8a2ec48d5cd3a300
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2011-10-15 01:52:28 +0200

    Added Pipeline class and stub of Tee class

---
+++ pipeline.py
@@ -0,0 +1,49 @@
+'''
+Copyright (C) 2011 Jesus Leganes "piranna", piranna@gmail.com
+
+This module is part of pypeline and is released under the BSD License:
+http://www.opensource.org/licenses/bsd-license.php.
+
+Created on 15/10/2011
+
+@author: piranna
+'''
+
+from types import GeneratorType
+
+
+class Pipeline(list):
+    """Pipe to content filters and process them sequentially
+
+    This pipe allow to content filters inside it and process them sequentially
+    using the output data of one of them as the input data of the next one.
+    Having the same calling restrictions that the filters, it's easy to use it
+    inside anothers pipeline objects as if it were a simple filter.
+    """
+
+    def __call__(self, stream):
+        """Run the pipeline
+
+        Return a static (non generator) version of the result
+        """
+
+        # Run the stream over all the filters on the pipeline
+        for filter in self:
+            stream = filter(stream)
+
+        # If last filter return a generator, staticalize it inside a list
+        if isinstance(stream, GeneratorType):
+            return list(stream)
+        return stream
+
+
+class Tee(set):
+    """Pipe that split the data stream to several pipes"""
+
+    def __call__(self, stream):
+        for data in stream:
+
+
+
+            while self._toSend:
+                self._toSend.pop()(data)
